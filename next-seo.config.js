518005c61b2fcf81ea71d0036193ab7756516b37
Stéphan CHRISTIAN <stephan.c@moramarket.mg>
2025-04-17 14:01:29 +0300

    fix(seo): ajust default seo image and url

--- next-seo.config.js
+++ next-seo.config.js
@@ -4,11 +4,11 @@ const SEO = {
     openGraph: {
         type: 'website',
         locale: 'fr_FR',
-        url: 'https://rotaractamontana.netlify.app',
+        url: 'https://rotaractamontana.vercel.app',
         site_name: 'Rotaract Amontana',
         images: [
             {
-                url: 'https://rotaractamontana.netlify.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FfanionRTCA.717e35b9.jpg&w=1200&q=75',
+                url: 'https://rotaractamontana.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FfanionRTCA.717e35b9.jpg&w=1200&q=75',
                 width: 1200,
                 height: 630,
                 alt: 'Rotaract Amontana logo',
