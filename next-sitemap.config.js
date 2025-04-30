04ff9c87ad91bda6599080c85a4a004d499a5f70
Stephan Christian <christianstephanjosue@gmail.com>
2025-04-30 16:01:17 +0300

    feat(seo): added next-sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,9 @@
 // next-sitemap.config.js
+import { NEXT_PUBLIC_URL } from "./utils/constants";
+
+
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'http://localhost:9220',
+    siteUrl: NEXT_PUBLIC_URL || 'http://localhost:9220',
     generateRobotsTxt: true, // (Optional) Generates a robots.txt file
     sitemapSize: 7000, // Number of URLs per sitemap file
   }
