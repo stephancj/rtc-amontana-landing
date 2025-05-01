b2365d155b14d7984ff6a2aab2d582f61ee58181
Stephan Christian <christianstephanjosue@gmail.com>
2025-05-01 07:51:51 +0300

    fix(seo):  add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="icon" href="/favicon.ico" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
         </Head>
         <body>
