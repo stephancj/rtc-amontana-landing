efee4255608cd97cde292888619fad576f38b1b3
Stephan Christian <christianstephanjosue@gmail.com>
2025-05-06 23:19:10 +0300

    add meta itemprop name

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="icon" href="/favicon.ico" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+          <meta itemprop="name" content="Rotaract Club Amontana" />
         </Head>
         <body>
           <Main />
